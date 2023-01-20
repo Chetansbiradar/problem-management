a9885cecbbd34b8d3dfccf3dde7f23096c2c77ff
Chetansbiradar <chetanhattoor@gmail.com>
2023-01-20 08:36:42 +0530

    added message.hbs as render

--- routes/auth.js
+++ routes/auth.js
@@ -4,64 +4,95 @@ const bcrypt = require("bcrypt");
 const jsonwebtoken = require("jsonwebtoken");
 
 router.get("/login", (req, res) => {
-    res.render("login.hbs");
+  res.render("login.hbs");
 });
 
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if(!email || !password) return res.send("Please fill all the fields");
+  if (!email || !password)
+    return res.render("message.hbs", {
+      title: "Error",
+      message: "Please fill all the fields",
+      link: "/auth/login",
+    });
 
-    const user  = await User.findOne({ email });
-    if(!user) return res.send("Invalid Username or Password");
+  const user = await User.findOne({ email });
+  if (!user)
+    return res.render("message.hbs", {
+      title: "Error",
+      message: "Invalid Username or Password",
+      link: "/auth/login",
+    });
 
-    //user type
-    const userType = user.role;
+  //check bcrypt password
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword)
+    return res.render("message.hbs", {
+      title: "Error",
+      message: "Invalid Username or Password",
+      link: "/auth/login",
+    });
 
-    //check bcrypt password
-    const validPassword = await bcrypt.compare(password, user.password);
-    if(!validPassword) return res.send("Invalid Username or Password");
-
-    //create and assign a token
-    const token = jsonwebtoken.sign({ _id: user._id }, "someSecretKey");
-    //send in cookie
-    res.cookie("authToken", token, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000
-    }).redirect("/");
+  //create and assign a token
+  const token = jsonwebtoken.sign({ _id: user._id }, "someSecretKey");
+  //send in cookie
+  res
+    .cookie("authToken", token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    })
+    .redirect("/");
 });
 
 router.get("/register", (req, res) => {
-    res.render("register.hbs");
+  res.render("register.hbs");
 });
 
 router.post("/register", (req, res) => {
-    const { name, email, password, phone } = req.body;
+  const { name, email, password, phone } = req.body;
 
-    if(!name || !email || !password || !phone) return res.send("Please fill all the fields");
-
-    //bcrypt password
-    const salt = bcrypt.genSaltSync(10);
-    const hashedPassword = bcrypt.hashSync(password, salt);
-
-    const user = new User({
-        name,
-        email,
-        phone,
-        "password": hashedPassword,
+  if (!name || !email || !password || !phone)
+    return res.render("message.hbs", {
+      title: "Error",
+      message: "Please fill all the fields",
+      link: "/auth/register",
     });
 
-    user.save( (err, user) => {
-        if(err) {
-            console.log(err);
-            return res.send("Something went wrong");
-        }
-        res.send("User registered successfully");
+  //bcrypt password
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  const user = new User({
+    name,
+    email,
+    phone,
+    password: hashedPassword,
+  });
+
+  user.save((err, user) => {
+    if (err) {
+      console.log(err);
+      return res.render("message.hbs", {
+        title: "Error",
+        message: "Something went wrong",
+        link: "/auth/register",
+        });
+    }
+    res.render("message.hbs", {
+      title: "Success",
+      message: "User registered successfully",
+      link: "/auth/login",
     });
+  });
 });
 
 router.get("/logout", (req, res) => {
-    res.clearCookie("authToken").redirect("/");
+  res.clearCookie("authToken").render("message.hbs", {
+    title: "Success",
+    message: "Logged out successfully",
+    link: "/auth/login",
+  });
 });
 
 module.exports = router;
